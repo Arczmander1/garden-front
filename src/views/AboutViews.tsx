8c4233bac66526c8ff0524e3801ce8782d03ca56
Arkadiusz Martenka <arkadiusz.martenka@gmail.com>
2022-07-15 18:17:51 +0200

    Added new content on web.

--- src/views/AboutViews.tsx
+++ src/views/AboutViews.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export const AboutView = () => (
     <div className="about">
-        <p>Jesteśmy pasieką z 20 letnim doświadczeniem. Tworzymy miody zgodnie ze starą Polską recepturą</p>
+        <h4>Jesteśmy pasieką z 20 letnim doświadczeniem. Tworzymy miody zgodnie ze starą Polską recepturą.</h4>
+        <br/>
+        <p> <b>Miody</b> tworzymy prowadząc gospodarke wędrowną, która umożliwia poszerzenie oferty o gatunki miodów, które nie występują na danym
+            terenie. Pozyskiwanymi odmianami miodów są: miód wielokwiatowy, mniszkowy, rzepakowy, leśny, lipowy, lipowo
+            - spadziowy, malinowy, gryczany, słonecznikowy, akacjowy, wrzosowy oraz miody spadziowe.</p>
     </div>
 );
--- src/views/ContactView.tsx
+++ src/views/ContactView.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 export const ContactView = () => (
     <div className="contact">
-        <h1>Numer telefonu: 696 169 692</h1>
-        <h1>Adres kontaktowy: arkadiusz.martenka@gmail.com</h1>
+        <h3>Numer telefonu: 696 169 692</h3>
+        <br/>
+        <h3>Adres email: arkadiusz.martenka@gmail.com</h3>
+        <br/>
+        <p>Adres: Kcynia, Szubinska 27</p>
+        <br/>
+        <p>Czynne pon-pt, godzina 8-18</p>
     </div>
 );
